db6ae3eb10a2e4c85662ceb6fa9e6490d89d9496
Dmitriy <klimov.dima@mail.ru>
2024-01-28 23:53:31 +0300

    HW_1

--- Homework/HW_1/task_1.js
+++ Homework/HW_1/task_1.js
@@ -11,3 +11,32 @@
 
 // • Реализуйте кастомный итератор для объекта musicCollection. Итератор должен перебирать альбомы по порядку.
 // • Используйте цикл for...of для перебора альбомов в музыкальной коллекции и вывода их на консоль в формате: Название альбома - Исполнитель (Год выпуска)
+
+const musicСompositions = [
+  { title: "Безграничный мир", author: "Александра", year: "2023" },
+  { title: "Эхо времени", author: "Сергей Иванов", year: "2021" },
+  { title: "Fahrenheit 451", author: "Наталья Петрова", year: "2022" },
+];
+
+const musicCollection = {
+  musicСompositions: [...musicСompositions],
+  [Symbol.iterator]: function () {
+    let countСompositions = 0;
+    return {
+      next: () => {
+        if (countСompositions >= this.musicСompositions.length) {
+          return { done: true };
+        } else {
+          return {
+            value: this.musicСompositions[countСompositions++],
+            done: false,
+          };
+        }
+      },
+    };
+  },
+};
+
+for (const music of musicCollection) {
+  console.log(music.title, music.author, music.year);
+}
