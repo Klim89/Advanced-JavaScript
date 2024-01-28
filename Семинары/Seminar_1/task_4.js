c362f2b0cd84af1e70c9fee19cfbf38ecffe99a5
Dmitriy <klimov.dima@mail.ru>
2024-01-28 22:50:26 +0300

    Семинар 1. Коллекции и итераторы. Модули

---
+++ Семинары/Seminar_1/task_4.js
@@ -0,0 +1,40 @@
+// Текст задания:
+// Представьте себе ситуацию: у нас есть группа студентов, и мы хотим отследить, кто из них посетил какие уроки и кто из преподавателей вёл данные уроки.
+// Map будет использоваться для хранения соответствия между уроком и преподавателем.
+// Set будет использоваться для хранения уникальных уроков, которые посетил каждый студент.
+
+// 1. Map: урок => преподаватель
+let lessons = new Map();
+// "Математика", "Смирнов"
+// "История", "Иванова"
+
+// 2. Map: студент => Set уроков
+
+// Проверка:
+console.log(`Преподаватель по Математике: ${lessons.get("Математика")}`); // Смирнов
+console.log(`Уроки Ивана: тут вывод уроков ивана`); // Математика, История
+
+function Student(name) {
+  this.name = name;
+  this.lessons = new Set();
+  this.addedLesson = function (lesson) {
+    this.lessons.add(lesson);
+  };
+}
+
+arrayPetrov = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const Petrov = new Student("Петров");
+const Ivanov = new Student("Иванов");
+arrayPetrov.forEach((item) => {
+  Petrov.addedLesson(item);
+  Ivanov.addedLesson(item);
+});
+
+let Students = new Map();
+Students.set(Petrov.name, Petrov.lessons);
+Students.set(Ivanov.name, Ivanov.lessons);
+
+// Проверка:
+console.log(`Преподаватель по Математике: ${lessons.get("Математика")}`); // Смирнов
+console.log(`Уроки Ивана: тут вывод уроков ивана ${Students}`); // Математика, История
+console.log(Students);
