aa98b8a4aaf5983cbf388a3c453ca3d7c2ac1ed3
Dmitriy <klimov.dima@mail.ru>
2024-01-31 00:21:29 +0300

    HW_2

--- Homework/HW_2/task_1.js
+++ Homework/HW_2/task_1.js
@@ -5,3 +5,61 @@
 // Реализуйте метод removeBook(title), который позволит удалять книгу из списка по названию. Если книги с таким названием нет в списке, выбросьте ошибку с соответствующим сообщением.
 // Реализуйте метод hasBook(title), который будет проверять наличие книги в библиотеке и возвращать true или false в зависимости от того, есть ли такая книга в списке или нет.
 // Реализуйте конструктор, который принимает начальный список книг (массив) в качестве аргумента. Убедитесь, что предоставленный массив не содержит дубликатов; в противном случае выбрасывайте ошибку.
+
+"use strict";
+
+class Library {
+  #books;
+  constructor(initialBooks = []) {
+    if (new Set(initialBooks).size !== initialBooks.length) {
+      throw new Error("Cписок книг содержит дубликаты");
+    }
+    this.#books = [...initialBooks];
+  }
+  get allBooks() {
+    return this.#books;
+  }
+
+  addBook(title) {
+    if (this.#books.includes(title)) {
+      throw new Error(
+        `Книга с названием "${title}" уже существует в библиотеке`
+      );
+    }
+    this.#books.push(title);
+  }
+
+  removeBook(title) {
+    const index = this.#books.indexOf(title);
+    if (index === -1) {
+      throw new Error(`Книги с названием "${title}" нет в библиотеке.`);
+    }
+    this.#books.splice(index, 1);
+  }
+
+  hasBook(title) {
+    return this.#books.includes(title);
+  }
+}
+
+try {
+  const library = new Library(["Book1", "Book2", "Book3"]);
+
+  console.log(library.allBooks);
+
+  library.addBook("Book4");
+  console.log(library.allBooks);
+
+  library.removeBook("Book2");
+  console.log(library.allBooks);
+
+  console.log(library.hasBook("Book1"));
+  console.log(library.hasBook("Book2"));
+
+  // Попытка добавить книгу с существующим названием
+  library.addBook("Book1");
+  // Попытка удалить несуществующую книгу
+  library.removeBook("Book5");
+} catch (error) {
+  console.error(error.message);
+}
